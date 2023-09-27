8d6ce6e53e1497c7e78f6fceaef2f34b2ba4c2c6
Anas0698 <anasnasikm54@gmail.com>
2023-09-27 20:19:30 +0500

    dartprojects

---
+++ main.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+void main (){
+// Q1.  Write a program that takes a list
+// of numbers as input and prints the even numbers in the list using a for loop.
+
+// List<int> list01 = [22,23,29,30,32,38,42,43,44,46,47];
+
+// for (var i in list01) if ( i % 2 == 0) {print("required numbers from $list01 are $i");}
+
+// Q2. Write a program that prints the
+// Fibonacci sequence up to a given number using a for loop.
+
+var fib = 5;
+for (var i = 1; i <= 10; i++);
+for (var j = 1; j <= i; j--);
+print(fib);
+
+// Q3. Implement a code that checks
+// whether a given number is prime or not
+
+
+// Q4.  Implement a code that finds the
+// largest element in a list using a for loop.
+
+// var list02=[3, 9, 1, 6, 4, 2, 8, 5, 7];
+// list02.sort();
+// print(list02.length);
+
+// Q5. Write a program that prints the
+// multiplication table of a given number using a for loop.
+
+// var tableof = 5;
+// for(var i = 1; i <= 10; i++ ){print("$tableof x $i = ${tableof* i}"); }
+
+// Q6. Implement a function that checks if a given string is a
+// palindrome.
+
+
+
+// Q7. Write a program to make such a
+// pattern like a right angle triangle with a number which will repeat a number in
+// a row. The pattern like 
+
+}
